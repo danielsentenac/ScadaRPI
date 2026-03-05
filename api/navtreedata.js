c97ad3c61eb82336778b9bbea939769f8ddb70ef
daniel.sentenac <daniel.sentenac@ligo.org>
2026-03-05 15:43:36 +0100

    Publish API docs (2026-03-05 14:43:36 UTC)

--- api/navtreedata.js
+++ api/navtreedata.js
@@ -38,26 +38,86 @@ var NAVTREE =
       [ "Notes for local testing", "index.html#autotoc_md10", null ]
     ] ],
     [ "Module API Sections", "md_build_api_docs__modules.html", [
-      [ "work_1500npanel", "md_build_api_docs__modules.html#autotoc_md12", null ],
-      [ "work_cbsaspanel", "md_build_api_docs__modules.html#autotoc_md13", null ],
-      [ "work_clim", "md_build_api_docs__modules.html#autotoc_md14", null ],
-      [ "work_cryo", "md_build_api_docs__modules.html#autotoc_md15", null ],
-      [ "work_cryo1500w", "md_build_api_docs__modules.html#autotoc_md16", null ],
-      [ "work_flowmeter", "md_build_api_docs__modules.html#autotoc_md17", null ],
-      [ "work_link", "md_build_api_docs__modules.html#autotoc_md18", null ],
-      [ "work_mainpanel", "md_build_api_docs__modules.html#autotoc_md19", null ],
-      [ "work_modulator", "md_build_api_docs__modules.html#autotoc_md20", null ],
-      [ "work_nepanel", "md_build_api_docs__modules.html#autotoc_md21", null ],
-      [ "work_panel", "md_build_api_docs__modules.html#autotoc_md22", null ],
-      [ "work_pcounter", "md_build_api_docs__modules.html#autotoc_md23", null ],
-      [ "work_pump_rack", "md_build_api_docs__modules.html#autotoc_md24", null ],
-      [ "work_rga", "md_build_api_docs__modules.html#autotoc_md25", null ],
-      [ "work_sqz", "md_build_api_docs__modules.html#autotoc_md26", null ],
-      [ "work_sqz0n", "md_build_api_docs__modules.html#autotoc_md27", null ],
-      [ "work_sqz300n", "md_build_api_docs__modules.html#autotoc_md28", null ],
-      [ "work_tube1500w", "md_build_api_docs__modules.html#autotoc_md29", null ],
-      [ "work_venting", "md_build_api_docs__modules.html#autotoc_md30", null ],
-      [ "work_wepanel", "md_build_api_docs__modules.html#autotoc_md31", null ]
+      [ "work_1500npanel", "md_build_api_docs__modules.html#autotoc_md12", [
+        [ "<a href=\"./work_1500npanel/annotated.html\">Classes</a>", "md_build_api_docs__modules.html#autotoc_md13", null ],
+        [ "<a href=\"./work_1500npanel/files.html\">Files</a>", "md_build_api_docs__modules.html#autotoc_md14", null ]
+      ] ],
+      [ "work_cbsaspanel", "md_build_api_docs__modules.html#autotoc_md15", [
+        [ "<a href=\"./work_cbsaspanel/annotated.html\">Classes</a>", "md_build_api_docs__modules.html#autotoc_md16", null ],
+        [ "<a href=\"./work_cbsaspanel/files.html\">Files</a>", "md_build_api_docs__modules.html#autotoc_md17", null ]
+      ] ],
+      [ "work_clim", "md_build_api_docs__modules.html#autotoc_md18", [
+        [ "<a href=\"./work_clim/annotated.html\">Classes</a>", "md_build_api_docs__modules.html#autotoc_md19", null ],
+        [ "<a href=\"./work_clim/files.html\">Files</a>", "md_build_api_docs__modules.html#autotoc_md20", null ]
+      ] ],
+      [ "work_cryo", "md_build_api_docs__modules.html#autotoc_md21", [
+        [ "<a href=\"./work_cryo/annotated.html\">Classes</a>", "md_build_api_docs__modules.html#autotoc_md22", null ],
+        [ "<a href=\"./work_cryo/files.html\">Files</a>", "md_build_api_docs__modules.html#autotoc_md23", null ]
+      ] ],
+      [ "work_cryo1500w", "md_build_api_docs__modules.html#autotoc_md24", [
+        [ "<a href=\"./work_cryo1500w/annotated.html\">Classes</a>", "md_build_api_docs__modules.html#autotoc_md25", null ],
+        [ "<a href=\"./work_cryo1500w/files.html\">Files</a>", "md_build_api_docs__modules.html#autotoc_md26", null ]
+      ] ],
+      [ "work_flowmeter", "md_build_api_docs__modules.html#autotoc_md27", [
+        [ "<a href=\"./work_flowmeter/annotated.html\">Classes</a>", "md_build_api_docs__modules.html#autotoc_md28", null ],
+        [ "<a href=\"./work_flowmeter/files.html\">Files</a>", "md_build_api_docs__modules.html#autotoc_md29", null ]
+      ] ],
+      [ "work_link", "md_build_api_docs__modules.html#autotoc_md30", [
+        [ "<a href=\"./work_link/annotated.html\">Classes</a>", "md_build_api_docs__modules.html#autotoc_md31", null ],
+        [ "<a href=\"./work_link/files.html\">Files</a>", "md_build_api_docs__modules.html#autotoc_md32", null ]
+      ] ],
+      [ "work_mainpanel", "md_build_api_docs__modules.html#autotoc_md33", [
+        [ "<a href=\"./work_mainpanel/annotated.html\">Classes</a>", "md_build_api_docs__modules.html#autotoc_md34", null ],
+        [ "<a href=\"./work_mainpanel/files.html\">Files</a>", "md_build_api_docs__modules.html#autotoc_md35", null ]
+      ] ],
+      [ "work_modulator", "md_build_api_docs__modules.html#autotoc_md36", [
+        [ "<a href=\"./work_modulator/annotated.html\">Classes</a>", "md_build_api_docs__modules.html#autotoc_md37", null ],
+        [ "<a href=\"./work_modulator/files.html\">Files</a>", "md_build_api_docs__modules.html#autotoc_md38", null ]
+      ] ],
+      [ "work_nepanel", "md_build_api_docs__modules.html#autotoc_md39", [
+        [ "<a href=\"./work_nepanel/annotated.html\">Classes</a>", "md_build_api_docs__modules.html#autotoc_md40", null ],
+        [ "<a href=\"./work_nepanel/files.html\">Files</a>", "md_build_api_docs__modules.html#autotoc_md41", null ]
+      ] ],
+      [ "work_panel", "md_build_api_docs__modules.html#autotoc_md42", [
+        [ "<a href=\"./work_panel/annotated.html\">Classes</a>", "md_build_api_docs__modules.html#autotoc_md43", null ],
+        [ "<a href=\"./work_panel/files.html\">Files</a>", "md_build_api_docs__modules.html#autotoc_md44", null ]
+      ] ],
+      [ "work_pcounter", "md_build_api_docs__modules.html#autotoc_md45", [
+        [ "<a href=\"./work_pcounter/annotated.html\">Classes</a>", "md_build_api_docs__modules.html#autotoc_md46", null ],
+        [ "<a href=\"./work_pcounter/files.html\">Files</a>", "md_build_api_docs__modules.html#autotoc_md47", null ]
+      ] ],
+      [ "work_pump_rack", "md_build_api_docs__modules.html#autotoc_md48", [
+        [ "<a href=\"./work_pump_rack/annotated.html\">Classes</a>", "md_build_api_docs__modules.html#autotoc_md49", null ],
+        [ "<a href=\"./work_pump_rack/files.html\">Files</a>", "md_build_api_docs__modules.html#autotoc_md50", null ]
+      ] ],
+      [ "work_rga", "md_build_api_docs__modules.html#autotoc_md51", [
+        [ "<a href=\"./work_rga/annotated.html\">Classes</a>", "md_build_api_docs__modules.html#autotoc_md52", null ],
+        [ "<a href=\"./work_rga/files.html\">Files</a>", "md_build_api_docs__modules.html#autotoc_md53", null ]
+      ] ],
+      [ "work_sqz", "md_build_api_docs__modules.html#autotoc_md54", [
+        [ "<a href=\"./work_sqz/annotated.html\">Classes</a>", "md_build_api_docs__modules.html#autotoc_md55", null ],
+        [ "<a href=\"./work_sqz/files.html\">Files</a>", "md_build_api_docs__modules.html#autotoc_md56", null ]
+      ] ],
+      [ "work_sqz0n", "md_build_api_docs__modules.html#autotoc_md57", [
+        [ "<a href=\"./work_sqz0n/annotated.html\">Classes</a>", "md_build_api_docs__modules.html#autotoc_md58", null ],
+        [ "<a href=\"./work_sqz0n/files.html\">Files</a>", "md_build_api_docs__modules.html#autotoc_md59", null ]
+      ] ],
+      [ "work_sqz300n", "md_build_api_docs__modules.html#autotoc_md60", [
+        [ "<a href=\"./work_sqz300n/annotated.html\">Classes</a>", "md_build_api_docs__modules.html#autotoc_md61", null ],
+        [ "<a href=\"./work_sqz300n/files.html\">Files</a>", "md_build_api_docs__modules.html#autotoc_md62", null ]
+      ] ],
+      [ "work_tube1500w", "md_build_api_docs__modules.html#autotoc_md63", [
+        [ "<a href=\"./work_tube1500w/annotated.html\">Classes</a>", "md_build_api_docs__modules.html#autotoc_md64", null ],
+        [ "<a href=\"./work_tube1500w/files.html\">Files</a>", "md_build_api_docs__modules.html#autotoc_md65", null ]
+      ] ],
+      [ "work_venting", "md_build_api_docs__modules.html#autotoc_md66", [
+        [ "<a href=\"./work_venting/annotated.html\">Classes</a>", "md_build_api_docs__modules.html#autotoc_md67", null ],
+        [ "<a href=\"./work_venting/files.html\">Files</a>", "md_build_api_docs__modules.html#autotoc_md68", null ]
+      ] ],
+      [ "work_wepanel", "md_build_api_docs__modules.html#autotoc_md69", [
+        [ "<a href=\"./work_wepanel/annotated.html\">Classes</a>", "md_build_api_docs__modules.html#autotoc_md70", null ],
+        [ "<a href=\"./work_wepanel/files.html\">Files</a>", "md_build_api_docs__modules.html#autotoc_md71", null ]
+      ] ]
     ] ]
   ] ]
 ];
